89dc06966712a174211be302446d8a93f4f7446c
quan091101 <nhquan0911@gmail.com>
2025-07-31 08:55:09 +0700

    Init Source
    Init Source
    
    .

---
+++ src/main/java/com/example/Immobi/ImmobiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Immobi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmobiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmobiApplication.class, args);
+	}
+
+}
